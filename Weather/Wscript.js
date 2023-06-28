59815894842a132e29c88fb2fbbcedb3b9168425
cchoi20 <60188850+cchoi20@users.noreply.github.com>
2023-06-28 00:46:31 -0400

    added wind&gust, air quality

--- Weather/Wscript.js
+++ Weather/Wscript.js
@@ -1,16 +1,55 @@
 const apiKey = '54500096dc0f4839bcb170952232306';
 
+// Reset values when page is loaded
+window.addEventListener('load', function() {
+  const locationInput = document.getElementById('location-input');
+  const checkbox1 = document.getElementById('checkbox1');
+  const checkbox2 = document.getElementById('checkbox2');
+  const checkbox3 = document.getElementById('checkbox3');
+
+  locationInput.value = '';
+  checkbox1.checked = false;
+  checkbox2.checked = false;
+  //checkbox3.checked = false;
+  //checkbox4.checked = false;
+});
+
+
 // Add button to add more weather containers to the list
 const addButton = document.getElementById('add-button');
 addButton.addEventListener('click', addWeatherContainer);
 
-// EL for the 'x' button to remove containers
+// EL for an 'x' button to remove containers
 document.addEventListener('click', function(event) {
     if (event.target.classList.contains('remove-button')) {
         event.target.parentNode.remove();
     }
 });
 
+// Checkbox check
+const checkboxWind = document.getElementById('checkbox1');
+const checkboxAQI = document.getElementById('checkbox2');
+//const checkboxVisibility = document.getElementById('checkbox3');
+//const checkboxForecast = document.getElementById('checkbox4');
+let isCheckbox1Checked = false;
+let isCheckbox2Checked = false;
+//let isCheckbox3Checked = false;
+//let isCheckbox4Checked = false;
+checkbox1.addEventListener('change', function() {
+  isCheckbox1Checked = checkbox1.checked;
+});
+checkbox2.addEventListener('change', function() {
+  isCheckbox2Checked = checkbox2.checked;
+});
+/*checkbox3.addEventListener('change', function() {
+  isCheckbox3Checked = checkbox3.checked;
+});
+/*checkbox4.addEventListener('change', function() {
+  isCheckbox4Checked = checkbox4.checked;
+});
+*/
+
+// Search if user presses enter on search bar
 const locationInput = document.getElementById('location-input');
 locationInput.addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
@@ -24,15 +63,19 @@ function addWeatherContainer() {
     const location = locationInput.value;
 
     // Create display container
-    const container = document.createElement('div');
-    container.className = 'weather-container';
+    const Maincontainer = document.createElement('div');
+    Maincontainer.className = 'main-container';
 
-    // Set text types
+    // Create Basic Weather stats container                                                 BASIC WEATHER
+    const Wcontainer = document.createElement('div');
+    Wcontainer.className = 'weather-container';
+
+    // Set text formatting
+    const localtimeElement = document.createElement('h3');
     const locationElement = document.createElement('h2');
     const countryElement = document.createElement('h3');
     const temperatureElement = document.createElement('p');
     const feelslikeElement = document.createElement('p');   // What temp the weather feels like
-    const localtimeElement = document.createElement('p');
 
     const descriptionContainer = document.createElement('div'); // Container for description and image
     const descriptionElement = document.createElement('h3');
@@ -48,23 +91,60 @@ function addWeatherContainer() {
     descriptionContainer.appendChild(imageElement);
 
     // Append elements to display container
-    container.appendChild(locationElement);
-    container.appendChild(countryElement);
-    container.appendChild(temperatureElement);
-    container.appendChild(feelslikeElement);
-    container.appendChild(localtimeElement);
-    container.appendChild(descriptionContainer);
+    Wcontainer.appendChild(localtimeElement);
+    Wcontainer.appendChild(locationElement);
+    Wcontainer.appendChild(countryElement);
+    Wcontainer.appendChild(temperatureElement);
+    Wcontainer.appendChild(feelslikeElement);
+    Wcontainer.appendChild(descriptionContainer);
 
-    // Create X button for user removal of containers
+
+    // Create another container for WIND and GUST statistics                        WIND & GUST
+    const WGcontainer = document.createElement('div');
+    WGcontainer.className = 'wind-gust-container';
+    WGcontainer.style.borderLeft = '1px solid #ccc'; // Border for aesthetics
+
+    // Set text formatting for WG container
+    const WGheader = document.createElement('h2');
+    WGheader.textContent = 'Wind & Gust';
+    const windspeedElement = document.createElement('p');
+    const winddescElement = document.createElement('p');
+    const winddirectionElement = document.createElement('p');
+    const gustspeedElement = document.createElement('p');
+
+    // Append elements to WG container
+    WGcontainer.appendChild(WGheader);
+    WGcontainer.appendChild(winddescElement);
+    WGcontainer.appendChild(windspeedElement);
+    WGcontainer.appendChild(winddirectionElement);
+    WGcontainer.appendChild(gustspeedElement);
+    
+
+    // Create another container for AIR QUALITY statistics                              AIR QUALITY
+    const AQcontainer = document.createElement('div');
+    AQcontainer.className = 'air-quality-container';
+    AQcontainer.style.borderLeft = '1px solid #ccc';
+
+    const AQheader = document.createElement('h2');
+    AQheader.textContent = 'Air Quality';
+    const aqtableElement = document.createElement('table');
+
+    // Append to AQ container
+    AQcontainer.appendChild(AQheader);
+    AQcontainer.appendChild(aqtableElement);
+
+
+    // Create X button for user removal of containers                               X Button
     const removeButton = document.createElement('button');
     removeButton.className = 'remove-button';
     removeButton.textContent = 'x';
-    container.appendChild(removeButton);
+    Maincontainer.appendChild(removeButton);
 
-    addButton.insertAdjacentElement('afterend', container);
+    // Put container after the add button                                           ADD MAIN CONTAINER
+    addButton.insertAdjacentElement('afterend', Maincontainer);
 
     // Update api Url
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
 
     fetch(apiUrl)
         .then(response => {
@@ -74,6 +154,7 @@ function addWeatherContainer() {
             return response.json();
         })
         .then(data => {
+            // Regular weather stats
             const location = data.location.name;
             const region = data.location.region;
             const country = data.location.country;
@@ -85,13 +166,113 @@ function addWeatherContainer() {
             const description = data.current.condition.text;
             const iconUrl = `https:${data.current.condition.icon}`;
 
+            // Get day from date
+            const date = new Date(localT.slice(0,10));
+            const day = date.toLocaleString('en-US', { weekday: 'long' });
+
             locationElement.textContent = `${location}, ${region}`;
             countryElement.textContent = `${country}`;
             temperatureElement.innerHTML = `<strong>Temperature:</strong> ${celc}°C, ${fahr}°F`;        // Make sure using ` not '
             feelslikeElement.innerHTML = `<strong>Feels like:</strong> ${flCelc}°C, ${flFahr}°F`;
-            localtimeElement.innerHTML = `<strong>Local Time: </strong> ${localT.slice(5, 7)}/${localT.slice(8, 10)}, ${localT.slice(11, 16)}`;
+            localtimeElement.innerHTML = `${day}, ${localT.slice(5, 7)}/${localT.slice(8, 10)} ${localT.slice(11, 16)}`;
             descriptionElement.textContent = description;
             imageElement.src = iconUrl;
+
+            Maincontainer.appendChild(Wcontainer);
+
+            // Wind & Gust stats
+            if(isCheckbox1Checked){
+                Maincontainer.appendChild(WGcontainer);
+
+                const windMPH = data.current.wind_mph;
+                const windKPH = data.current.wind_kph;
+                const windDegree = data.current.wind_degree;
+                const windDirection = data.current.wind_dir;
+                const gustMPH = data.current.gust_mph;
+                const gustKPH = data.current.gust_kph;
+
+                // Determine description based on speed
+                const windNum = parseFloat(windMPH);
+                if(windNum <= 1) winddescElement.innerHTML = `<strong>BF: 0,</strong> Calm`;
+                else if(windNum <= 3) winddescElement.innerHTML = `<strong>BF: 1,</strong> Light air`;
+                else if(windNum <= 7) winddescElement.innerHTML = `<strong>BF: 2,</strong> Light breeze`;
+                else if(windNum <= 12) winddescElement.innerHTML = `<strong>BF: 3,</strong> Gentle breeze`;
+                else if(windNum <= 18) winddescElement.innerHTML = `<strong>BF: 4,</strong> Moderate breeze`;
+                else if(windNum <= 24) winddescElement.innerHTML = `<strong>BF: 5,</strong> Fresh breeze`;
+                else if(windNum <= 31) winddescElement.innerHTML = `<strong>BF: 6,</strong> Strong breeze`;
+                else if(windNum <= 38) winddescElement.innerHTML = `<strong>BF: 7,</strong> Moderate gale`;
+                else if(windNum <= 46) winddescElement.innerHTML = `<strong>BF: 8,</strong> Fresh gale`;
+                else if(windNum <= 54) winddescElement.innerHTML = `<strong>BF: 9,</strong> Strong gale`;
+                else if(windNum <= 63) winddescElement.innerHTML = `<strong>BF: 10,</strong> Whole gale`;
+                else if(windNum <= 72) winddescElement.innerHTML = `<strong>BF: 11,</strong> Violent storm`;
+                else if(windNum >= 73) winddescElement.innerHTML = `<strong>BF: 12,</strong> Hurricane`;
+
+                windspeedElement.innerHTML = `<strong>Wind Speed:</strong> ${windMPH} mph, ${windKPH} kph`;
+                // Wind description done above
+                winddirectionElement.innerHTML = `<strong>Wind Direction:</strong> ${windDirection}, ${windDegree}°`;
+                gustspeedElement.innerHTML = `<strong>Gust Speed:</strong> ${gustMPH}mph, ${gustKPH}kph`;
+            }
+            
+            // Air quality stats
+            if(isCheckbox2Checked){
+                Maincontainer.appendChild(AQcontainer);
+
+                const airqual = data.current.air_quality;
+                
+                for (const key in airqual) {
+                    const value = airqual[key];
+                    
+                    const row = document.createElement('tr');
+                    const keyCell = document.createElement('td');
+                    const valueCell = document.createElement('td');
+
+                    if(key === 'co'){
+                        keyCell.innerHTML = `<strong>Carbon Monoxide (μg/m3): </strong>`;
+                        valueCell.textContent = value.toFixed(2);
+                    }
+                    else if(key === 'no2'){
+                        keyCell.innerHTML = `<strong>Nitrogen Dioxide (μg/m3): </strong>`;
+                        valueCell.textContent = value.toFixed(2);
+                    }
+                    else if(key === 'o3'){
+                        keyCell.innerHTML = `<strong>Ozone (μg/m3): </strong>`;
+                        valueCell.textContent = value.toFixed(2);
+                    }
+                    else if(key === 'so2'){
+                        keyCell.innerHTML = `<strong>Sulphur Dioxide (μg/m3): </strong>`;
+                        valueCell.textContent = value.toFixed(2);
+                    }
+                    else if(key === 'pm2_5'){
+                        keyCell.innerHTML = `<strong>PM2.5: </strong>`;
+                        valueCell.textContent = value.toFixed(2);
+                    }
+                    else if(key === 'pm10'){
+                        keyCell.innerHTML = `<strong>PM10: </strong>`;
+                        valueCell.textContent = value.toFixed(2);
+                    }
+                    else if(key === 'us-epa-index'){
+                        keyCell.innerHTML = `<strong>US EPA Index: </strong>`;
+                        if(value === 1) valueCell.textContent = `1, Good`;
+                        else if(value === 2) valueCell.textContent = `2, Moderate`;
+                        else if(value === 3) valueCell.textContent = `3, Unhealthy for Sensitive Groups`;
+                        else if(value === 4) valueCell.textContent = `4, Unhealthy`;
+                        else if(value === 5) valueCell.textContent = `5, Very Unhealthy`;
+                        else if(value === 6) valueCell.textContent = `6, Hazardous`;
+                    }
+                    else if(key === 'gb-defra-index'){
+                        keyCell.innerHTML = `<strong>UK Defra Index: </strong>`;
+                        if(value <= 3) valueCell.textContent = `${value}, Low`;
+                        else if(value <= 6) valueCell.textContent = `${value}, Moderate`;
+                        else if(value <= 9) valueCell.textContent = `${value}, High`;
+                        else if(value >= 10) valueCell.textContent = `${value}, Very High`;
+                    }
+                  
+                    row.appendChild(keyCell);
+                    row.appendChild(valueCell);
+                    aqtableElement.appendChild(row);
+                }
+               
+            }
         })
         .catch(error => {
             console.error('Error:', error);
